a79092adcd68898c8a7c4efae24eb97998a5266c
Zukky55 <www.kase@gmail.com>
2018-09-06 14:36:08 +0900

    enemy,Player,Stage,Gimmickの枠組み作成

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>Enemy's status</summary>
+[System.Serializable]
+struct EnemyStatus
+{
+    /// <summary>Player's speed</summary>
+    public float speed;
+}
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class EnemyController : MonoBehaviour
+{
+    /// <summary>Start position</sumTmary>
+    [SerializeField] Vector3 m_startPos;
+    /// <summary>Position at end of frame</sumTmary>
+    [SerializeField] Vector3 m_endPos;
+    /// <summary>Set time</summary>
+    [SerializeField] float m_time;
+    /// <summary>Amount of movement per second</summary>
+    private Vector3 m_deltaPos;
+    /// <summary>Elapsed time count</summary>
+    private float m_elapsedTime;
+    /// <summary>Fragment of end</summary>
+    private bool m_directionToggle = true;
+    /// <summary>Rigidbody2D</summary>
+//    Rigidbody2D m_rb2d;
+
+    private void Start()
+    {
+        //rigidbody2d取得
+//        m_rb2d = GetComponent<Rigidbody2D>();
+        // StartPosをオブジェクトに初期位置に設定
+        transform.position = m_startPos;
+        // 1秒当たりの移動量を算出
+        m_deltaPos = (m_endPos - m_startPos) / m_time;
+        m_elapsedTime = 0;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, m_startPos);
+    }
+
+    void Update()
+    {
+        // 1秒当たりの移動量にTime.deltaTimeを掛けると1フレーム当たりの移動量となる
+        // Time.deltaTimeは前回Updateが呼ばれてからの経過時間
+        transform.position += m_deltaPos * Time.deltaTime;
+        // 往路復路反転用経過時間
+        m_elapsedTime += Time.deltaTime;
+        // 移動開始してからの経過時間がtimeを超えると往路復路反転
+        if (m_elapsedTime > m_time)
+        {
+            if (m_directionToggle)
+            {
+                // StartPos→EndPosだったので反転してEndPos→StartPosにする
+                // 現在の位置がEndPosなので StartPos - EndPosでEndPos→StartPosの移動量になる
+                m_deltaPos = (m_startPos - m_endPos) / m_time;
+                // 誤差があるとずれる可能性があるので念のためオブジェクトの位置をEndPosに設定
+                transform.position = m_endPos;
+                transform.rotation = Quaternion.FromToRotation(Vector3.up, m_endPos);
+            }
+            else
+            {
+                // m_endPos→m_startPosだったので反転してにm_startPos→m_endPosする
+                // 現在の位置がm_startPosなので m_endPos - m_startPos→m_endPosの移動量になる
+                m_deltaPos = (m_endPos - m_startPos) / m_time;
+                // 誤差があるとずれる可能性があるので念のためオブジェクトの位置をSrartPosに設定
+                transform.position = m_startPos;
+                transform.rotation = Quaternion.FromToRotation(Vector3.up, m_startPos);
+            }
+            // 往路復路のフラグ反転
+            m_directionToggle = !m_directionToggle;
+            // 往路復路反転用経過時間クリア
+            m_elapsedTime = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>Player type</summary>
+public enum Type
+{
+    small, medium, large
+}
+
+/// <summary>Player's ability</summary>
+public enum Ability
+{
+    thorough, destroy, highSpeed
+}
+/// <summary>Status of each player type</summary>
+[System.Serializable]
+struct MyStatus
+{
+    /// <summary>Player's speed</summary>
+    public float speed;
+    /// <summary>Player's weight</summary>
+    public int weight;
+    /// <summary>Ink gauge</summary>
+    public float inkGuage;
+}
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>Status of each player type</summary>
+    [SerializeField] MyStatus m_MyStatus;
+    /// <summary>player's type</summary>
+    [SerializeField] Type m_type;
+    /// <summary>player's ability</summary>
+    [SerializeField] Ability m_ability;
+    /// <summary>Rigidbody2D</summary>
+    Rigidbody2D m_rb2d;
+    /// <summary>Position of the previous frame</summary>
+    private Vector3 m_offsetPos;
+
+    /// <summary>Mover</summary>
+    void Move()
+    {
+        //x,y軸を入力しそのベクトルを取得,速度に代入
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(x, y);
+        m_rb2d.velocity = direction * m_MyStatus.speed;
+        //前フレームのオフセットを取り、現フレームとの差異が一定量以上ある時だけ回転する様にして差異が0の時に上を向いてしまわない様にする
+        Vector3 diff = transform.position - m_offsetPos;
+        if (diff.magnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.FromToRotation(Vector2.up, direction);
+        }
+        m_offsetPos = transform.position;
+    }
+    /// <summary>Change for each type</summary>
+    void Initialize()
+    {
+        switch(m_type)
+        {
+            case Type.small:
+                Debug.Log("this object is small type");
+                m_MyStatus.speed = 5;
+                m_MyStatus.weight = 1;
+                break;
+            case Type.medium:
+                m_MyStatus.speed = 4;
+                m_MyStatus.weight = 3;
+                break;
+            case Type.large:
+                m_MyStatus.speed = 3;
+                m_MyStatus.weight = 5;
+                break;
+            default:
+                Debug.Log("m_type is not set properly!!");
+                break;
+        }
+
+    }
+
+    void Start()
+    {
+        Initialize();
+        m_rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+
+
+    }
+
+}
